030103f42ea1856b890ca2a012ec241a405998de
Antoine07 <antoine.lucsko@gmail.com>
2024-09-02 14:45:51 +0200

    type SQL

--- 01_SQL/Supports/Chap2_CREATE/Corrections/exercices.sql
+++ 01_SQL/Supports/Chap2_CREATE/Corrections/exercices.sql
@@ -19,3 +19,35 @@ DROP COLUMN `num_street` ;
 ALTER TABLE `companies`
 ADD COLUMN `num_street` SMALLINT  
 AFTER `name`;
+
+
+CREATE TABLE `pilots` (
+    `certificate` VARCHAR(6),
+    `num_flying` DECIMAL(7,1),
+    `company` CHAR(4),
+    `name` VARCHAR(20) NOT NULL,
+    CONSTRAINT pk_pilots PRIMARY KEY (`certificate`)
+) ENGINE=InnoDB ;
+
+-- alter 
+ALTER TABLE `pilots` ADD CONSTRAINT `un_name` UNIQUE (`name`);
+
+-- directement dans à la création de la table
+
+DROP TABLE IF EXISTS `pilots` ;
+CREATE TABLE `pilots` (
+    `certificate` VARCHAR(6),
+    `num_flying` DECIMAL(7,1),
+    `company` CHAR(4),
+    `name` VARCHAR(20) NOT NULL,
+    CONSTRAINT un_name UNIQUE (`name`),
+    CONSTRAINT pk_pilots PRIMARY KEY (`certificate`)
+) ENGINE=InnoDB ;
+
+INSERT INTO pilots SET `name`= 'Alan', certificate='cert-1' ;
+-- ne marche pas car contrainte d'unicité sur la colonne name 
+INSERT INTO pilots SET `name`= 'Alan', certificate='cert-2' ;
+
+-- suppression de la contrainte UNIQUE sur la colonne name 
+
+ALTER TABLE `pilots` DROP CONSTRAINT `un_name` ;
